d0d879ca2b3a410805aa58f12e1ddb60ecd15845
Mark <mollerenshaw@zepler.net>
2015-10-29 13:00:10 +0000

    First version of step definitions.

---
+++ features/step_definitions/listFilms.js
@@ -0,0 +1,18 @@
+module.exports = function () {
+
+    this.Given(/^X$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback.pending();
+    });
+
+    this.When(/^Y$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback.pending();
+    });
+
+    this.Then(/^Z$/, function (callback) {
+        // Write code here that turns the phrase above into concrete actions
+        callback.pending();
+    });
+
+};
