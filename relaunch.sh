53c03bc8fdb478375fdcee5edb169e63107941e1
Mark <mollerenshaw@zepler.net>
2016-04-10 20:35:20 +0100

    Container launch script now includes mongo environment variables.

--- relaunch.sh
+++ relaunch.sh
@@ -5,4 +5,10 @@ docker rm -f fs-api
 
 docker build -t mollerenshaw/filmsite-backend .
 
-docker run -d -p 8080:8080 --name fs-api mollerenshaw/filmsite-backend
+docker run -d \
+    -p 8080:8080 \
+    -e MONGO_HOST='mongodb://192.168.99.100:27017' \
+    -e MONGO_USER='some_mongo_user' \
+    -e MONGO_PASSWORD='some_mongo_password' \
+    --name fs-api \
+    mollerenshaw/filmsite-backend
