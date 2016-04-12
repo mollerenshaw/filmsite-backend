92858d0f6e5a0b58ec68f47cd3185d35b8b81a63
Mark <mollerenshaw@zepler.net>
2016-04-12 21:03:30 +0100

    Docker now uses special docker network, and correct connection details passed in for the Mongo DB.

--- relaunch.sh
+++ relaunch.sh
@@ -7,8 +7,9 @@ docker build -t mollerenshaw/filmsite-backend .
 
 docker run -d \
     -p 8080:8080 \
-    -e MONGO_HOST='mongodb://192.168.99.100:27017' \
+    -e MONGO_HOST='mongodb://fs-db:27017' \
     -e MONGO_USER='some_mongo_user' \
     -e MONGO_PASSWORD='some_mongo_password' \
+    --net=filmsite-network \
     --name fs-api \
     mollerenshaw/filmsite-backend
