325525c8176649025531f06fabd6a6684991dac8
Mark <mollerenshaw@zepler.net>
2016-04-14 10:31:34 +0100

    Ran js_beautify

--- test/bridges/omdbBridgeSpec.js
+++ test/bridges/omdbBridgeSpec.js
@@ -1,16 +1,14 @@
 var expect = require("chai").expect;
 var omdbBridge = require("../../lib/bridges/omdbBridge.js");
 
-
 context("OMDB Bridge", function () {
   describe("#getFilmByTitle()", function () {
     it("should include the title in the options", function () {
       expect(omdbBridge.getFilmByTitle("Jaws")).to.equal("Wibble");
     });
-      
-    it("should refuse empty film titles", function() {
-          
-              
+
+    it("should refuse empty film titles", function () {
+
     });
   });
 });
