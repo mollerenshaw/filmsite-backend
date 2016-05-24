47a2e3f1c061b33f91b6702ab2808d17aee1697e
Mark <mollerenshaw@zepler.net>
2016-05-24 19:20:31 +0100

    Added initial WORKING feature file and step definitions for seasons!

---
+++ features/step_definitions/listSeasonsSteps.js
@@ -0,0 +1,63 @@
+'use strict';
+
+var request = require('request');
+
+
+module.exports = function () {
+
+  var returnedResources;
+    
+  this.Given(/^the system knows about the following (.+)s:$/, function (resourceType, resources, callback) {
+
+    console.log("GIVEN: " + resourceType);
+
+    // Always finish with a call to callback().
+    callback();
+  });
+
+  this.When(/^the client requests a list of (.+)s$/, function (resourceType, callback) {
+
+    var options = httpOptions(resourceType);
+
+    var cb = function (error, response, body) {
+
+      if (!error && response.statusCode == 200) {
+        returnedResources = JSON.parse(body);
+      }
+      callback();
+    };
+      
+    request(options, cb);
+
+  });
+    
+  this.Then(/^the response is a list containing ([0-9]+) (.+)s$/, function (resourceCount, resourceType, callback) {
+ 
+    console.log("Then there should be " + resourceCount + " " + resourceType + "s.");
+
+    console.log("Res: " + returnedResources);
+      
+    // Always finish with a call to callback().
+    callback();
+  });
+
+  this.Then(/^one (.+) has the following attributes:$/, function (resourceType, attributes, callback) {
+ 
+    console.log("ATTRS");
+      
+    // Always finish with a call to callback().
+    callback();
+  });
+    
+  /**
+   * Constructs HTTP options for a request of a given type against a given end point.
+   */
+  var httpOptions = function(resourceType) {
+
+    var options = {
+      url: 'http://192.168.99.100:8080/api/' + resourceType
+    };
+    
+    return options;
+  };
+};
